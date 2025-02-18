fe4060c448417540360300cee4b00c3efcd84743
Evgeniy <evgeniipusser88@gmail.com>
2025-02-18 02:12:58 +0200

    optimized with new function, making all the actions of finding current min? max? count and sum

--- index.js
+++ index.js
@@ -1,46 +1,34 @@
 function minMaxAverage(...operands) {
-  let result = [Infinity, -Infinity, 0];
-  let sum = 0;
-  let counter = 0;
+  let min = Infinity,
+    max = -Infinity,
+    sum = 0,
+    count = 0;
+
+  function processNumber(num) {
+    if (num < min) min = num;
+    if (num > max) max = num;
+    sum += num;
+    count++;
+  }
 
   for (let i = 0; i < operands.length; i++) {
-    let operandA = operands[i];
+    let operand = operands[i];
 
-    if (Array.isArray(operandA)) {
-      let [min, max, sumAr, counterAr] = minMaxArrayAverage(operandA);
-      if (min < result[0]) result[0] = min;
-      if (max > result[1]) result[1] = max;
-      sum += sumAr;
-      counter += counterAr;
-    } else if (operandA / operandA === 1) {
-      if (operandA < result[0]) result[0] = operandA;
-      if (operandA > result[1]) result[1] = operandA;
-      sum += operandA;
-      counter++;
+    if (Array.isArray(operand)) {
+      for (let j = 0; j < operand.length; j++) {
+        processNumber(operand[j]);
+      }
+    } else if (operand / operand === 1) {
+      processNumber(operand);
     }
   }
 
-  result[2] = counter > 0 ? Math.round(sum / counter) : undefined;
-
-  return result;
+  return [
+    min,
+    max,
+    count > 0 ? Math.round((sum / count) * 100) / 100 : undefined,
+  ];
 }
-
-function minMaxArrayAverage(array) {
-  let sumAr = 0;
-  let counterAr = 0;
-  let min = Infinity;
-  let max = -Infinity;
-
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] < min) min = array[i];
-    if (array[i] > max) max = array[i];
-    sumAr += array[i];
-    counterAr++;
-  }
-
-  return [min, max, sumAr, counterAr];
-}
-
 
 const result = minMaxAverage(1, 2, 3, 10, [8, -900], 0);
 
@@ -52,4 +40,10 @@ const result1 = minMaxAverage([1, 2], 3, 10, [8, -900], 0);
 
 console.log(
   `The output of function "minMaxAverage" with numbers and arrays as arguments: (1, 2, 3, 10, [8, -900], 0) will be: \n Min Value: ${result1[0]} \n Max Value: ${result1[1]} \n Average Value: ${result1[2]}`
+);
+
+const result2 = minMaxAverage([1.89, -2.88, 3], 777, [8, -100], 0, "Atom");
+
+console.log(
+  `The output of function "minMaxAverage" with numbers and arrays as arguments: ([1.89, -2.88, 3], 777, [8, -100], 0, "Atom") will be: \n Min Value: ${result2[0]} \n Max Value: ${result2[1]} \n Average Value: ${result2[2]}`
 );
